30989044f19789c14019afb85769aadd60019e97
David Salomon <david35mm@pm.me>
2021-05-28 23:48:25 -0400

    Final Update

--- repogen.sh
+++ repogen.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-destdir="$(pwd)"
-for arch in SRPMS aarch64 noarch x86_64
-do
-	pushd ${destdir}/${arch} >/dev/null 2>&1
-		createrepo --database --update --xz --deltas .
-	popd >/dev/null 2>&1
-done
+createrepo --database --update --xz --deltas .
