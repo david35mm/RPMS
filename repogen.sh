601d16772d88277ee0253b7ab21e35f40789e3b5
David Salomon <david35mm@pm.me>
2021-05-28 23:19:30 -0400

    Repo ready for primetime

--- repogen.sh
+++ repogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 destdir="/var/ftp/repo/Fedora/10"
-for arch in i386 x86_64
+for arch in aarch64 x86_64 SRPMS
 do
 	pushd ${destdir}/${arch} >/dev/null 2>&1
 		createrepo --database --update --xz --deltas .
