7a6dd8b496a89c08265c21a8f23be9170f3fc1ab
David Salomon <david35mm@pm.me>
2021-06-01 21:38:44 -0400

    Repo with public PGP key and rpms signed

--- repogen.sh
+++ repogen.sh
@@ -3,7 +3,7 @@
 for arch in SRPMS aarch64 noarch x86_64
 do
 	cd $(pwd)/${arch}
-		rpmsign --addsign *.rpm && cd ..
+		rpmsign --addsign *.rpm
 done
 
 createrepo --database --update --xz --deltas .
